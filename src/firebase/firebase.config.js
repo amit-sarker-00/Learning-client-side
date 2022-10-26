24e6da86ff0e58edbe38543a621928784a8387ad
Amit Sarker <amitsarker255@gmail.com>
2022-10-26 20:21:35 +0600

    router setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGm39V0qfVrOc645cr5ZsOhLuUIztJUC4",
+  authDomain: "educational-services-d9f7c.firebaseapp.com",
+  projectId: "educational-services-d9f7c",
+  storageBucket: "educational-services-d9f7c.appspot.com",
+  messagingSenderId: "84942232956",
+  appId: "1:84942232956:web:41360083c426958a20d8f9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
