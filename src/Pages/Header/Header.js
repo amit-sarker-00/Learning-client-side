9723af5a7e63688bd78607a9935b4493dd63818f
Amit Sarker <amitsarker255@gmail.com>
2022-10-27 19:15:52 +0600

    toggle button added

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaBookReader, FaUser } from "react-icons/fa";
+import { FaBookReader } from "react-icons/fa";
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useState } from "react";
 
 const Header = () => {
+  // this state for toggle
+  const [toggle, setToggle] = useState(false);
   const { user, logOut } = useContext(AuthContext);
   console.log(user);
   const handelLogOut = () => {
@@ -54,7 +57,22 @@ const Header = () => {
             <Link to="/blog">Blog</Link>
           </li>
           <li>
-            <button>Toggle button</button>
+            <div className="flex">
+              <label class="inline-flex relative items-center mr-5 cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer "
+                  checked={toggle}
+                  readOnly
+                />
+                <div
+                  onClick={() => {
+                    setToggle(!toggle);
+                  }}
+                  className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+                ></div>
+              </label>
+            </div>
           </li>
         </ul>
       </div>
--- src/Shared/ErrorPage/ErrorPage.js
+++ src/Shared/ErrorPage/ErrorPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <div>
-      <h1>This is error page</h1>
+      <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+          Page Not Found
+        </div>
+        <div>
+          <button className="btn btn-info my-10">
+            <Link to="/home">Back To Home</Link>
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
