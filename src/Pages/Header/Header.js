858f5689ee7b5652d0a3a696f6bcad1406db4241
Amit Sarker <amitsarker255@gmail.com>
2022-10-28 22:38:02 +0600

    navbar problem fixed

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -125,6 +125,65 @@ const Header = () => {
           <p>EduChamp</p>
         </Link>
       </div>
+
+      <div className="hidden lg:flex">
+        <ul
+          tabIndex={0}
+          className=" flex mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li className="mr-10">
+            <Link
+              to="/home"
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="mr-10">
+            <Link
+              to="/courses"
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+            >
+              Courses
+            </Link>
+          </li>
+
+          <li className="mr-10">
+            <Link
+              to="/faq"
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+            >
+              FAQ
+            </Link>
+          </li>
+          <li className="mr-10">
+            <Link
+              to="/blog"
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+            >
+              Blog
+            </Link>
+          </li>
+          <li className="mr-10">
+            <div className="flex">
+              <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer "
+                  checked={toggle}
+                  readOnly
+                />
+                <div
+                  onClick={() => {
+                    setToggle(!toggle);
+                  }}
+                  className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+                ></div>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </div>
       <div className="navbar-center hidden lg:flex"></div>
       <>
         {user?.uid ? (
